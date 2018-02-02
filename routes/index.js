dc72db56e4f45d8b975aba89a223b7559684e48f
unknown <yuriy.bratko@gmail.com>
2018-02-02 22:59:57 +0200

    First version of application moved into github

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+var data = fs.readFileSync('brands.json');
+var brands = JSON.parse(data);
+console.log(brands);
+var size = Object.keys(brands).length;
+console.log('size of my object is: ' + size);
+// returns array of keys
+var brandList = Object.keys(brands);
+// keys.length is ...
+console.log(brandList, brandList.length);
+
+// iterating through values in our json
+brandList.forEach(function(brand) {
+  var items = Object.keys(brands[brand]);
+  items.forEach(function(item) {
+    var value = brands[brand][item];
+    console.log(brand+': '+item+' = '+value);
+  });
+});
+
+
+
+//var targetBrandName = "ABC";
+//console.log('variable targetBrandName: ' + targetBrandName);
+if(brands.ABC) {
+  console.log('true', brands.ABC.WebsiteProdVersion);
+ }
+
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { "brandList": brandList, json: brands });
+});
+
+module.exports = router;
---
+++ routes/update.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+//
+const updateJsonFile = require('update-json-file')
+const filePath = './brands.json'
+//
+
+
+/* POST data updates */
+router.post('/', function(req, res, next) {
+  var versionToInsert = JSON.stringify(req.body.OTC.BOProdVersion, null, 2)
+  console.log('Updated version: ' + versionToInsert)
+  updateJsonFile(filePath, (data) => {
+  data.OTC.BOProdVersion = versionToInsert
+  return data
+})
+  
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+  //res.send('data updated');
+});
+
+module.exports = router;
+
+//for workig with TFS hook
+//need to see real hook example
+/*
+  var brandName = JSON.stringify(req.body.blah.blah.environmentName, null, 2)
+  var versionNumber = JSON.stringify(req.body.blah.blah.environmentName.versionNumber, null, 2)
+  updateJsonFile(filePath, (data) => {
+  data.brandName.versionNumber = versionNumber //?????
+  return data
+})
+*/
