fe21abe0b1f28c617b63b35d2557dfdc82ee8131
unknown <yuriy.bratko@gmail.com>
2018-04-05 16:59:25 +0300

    green clolor for success

--- routes/success.js
+++ routes/success.js
@@ -9,7 +9,7 @@ var options = {
     "attachments": [
         {
             "fallback": "Required plain-text summary of the attachment.",
-            "color": "#ff0000",
+            "color": "#00ff00",
             "image_url": "https://lh3.googleusercontent.com/79GExvi6WvkQacZ0SNNCNzsvpMpcXpCD_rLD7r72bkvNu2GEKFFg9CZdp4UTaU09F43xrdhlIf96dddDiKnlNJ1NlXKgFJrTPHG2z9QcipsGf1xvIVl6k-bOCQlOvnkERANs9clwYsMDas1h-RA43xEcjHiTc8JiWnjD6YeDS_J52Lw0rh4nosmqKRMzOyY9EMn7GmOX6GP3nHY06uqsuIeN267PBqvyrGFwYyc8aa-ebEsy7vrQ3XAOKJDAGLqjMaj-qzdKoeMYdB0NG4Y57Zs89uxQrGfumGybEKolacaCpZyZS1M7rdOUtwaYNAaTuddtumVk4Joysz7XFEQbBI0O8SKbRfK1Kc-XwT32YW6MdmPHRD0VlaQTTunfoTZmpXYM8huEiQxoZfAX_kIBx8TO8nycvKpyYt4DQQNQS5nJW_IgtHkeXN5mcpF-4h-vNsISaLK-LkUBfs90n6xj_vUef87UtgTYL8tDYAXxB5CmlYR56rLLUfVEg8lwGmlf-hqdzSBVegQHecvB9OIZCJt8zvkphY4OVz0HcRLeKRjFVSxMTdzzGgZ2Q8vVlGCdCVARJPBw69hWo7EziZMLC0jUlvw1rdp1DzcTRiMK0WC1UWDyGfi4QoT5CudoFOeUsPAleljzkir8XG81wVLklQc8Xpv2wgzjBA=s580-no"
         }
     ]}
